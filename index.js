f99ca73b2f6430e3e87a5d4077c37603c80aba57
Zenok4 <dungk4.camlo@gmail.com>
2023-09-06 19:53:05 +0700

    api places

--- index.js
+++ index.js
@@ -123,13 +123,13 @@ app.post("/places", (req, res) => {
   const {
     title,
     address,
-    addedPhoto,
+    addedPhotos,
     description,
     perks,
     extraInfo,
-    chechIn,
+    checkIn,
     checkOut,
-    maxGuest,
+    maxGuests,
   } = req.body;
   jwt.verify(token, jwtSecret, {}, async (err, userData) => {
     if (err) throw err;
@@ -138,16 +138,65 @@ app.post("/places", (req, res) => {
       owner: userData.id,
       title,
       address,
-      addedPhoto,
+      photos: addedPhotos,
       description,
       perks,
       extraInfo,
-      chechIn,
+      checkIn,
       checkOut,
-      maxGuest,
+      maxGuests,
     });
 
-    res.json(placeDoc)
+    res.json(placeDoc);
+  });
+});
+
+app.get("/places", (req, res) => {
+  const { token } = req.cookies;
+  jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+    const { id } = userData;
+    res.json(await Places.find({ owner: id }));
+  });
+});
+
+app.get("/places/:id", async (req, res) => {
+  const { id } = req.params;
+  res.json(await Places.findById(id));
+});
+
+app.put('/places/:id', async (req, res) => {
+  const { token } = req.cookies;
+  const {
+    id,
+    title,
+    address,
+    addedPhotos,
+    description,
+    perks,
+    extraInfo,
+    checkIn,
+    checkOut,
+    maxGuests,
+  } = req.body;
+  jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+    if (err) throw err;
+    const placeDoc = await Place.findById(id);
+    if (userData.id === placeDoc.owner.toString()) {
+      placeDoc.set({
+        title,
+        address,
+        photos: addedPhotos,
+        description,
+        perks,
+        extraInfo,
+        checkIn,
+        checkOut,
+        maxGuests,
+        price,
+      });
+      await placeDoc.save();
+      res.json("ok");
+    }
   });
 });
 
--- models/Place.js
+++ models/Place.js
@@ -4,13 +4,13 @@ const placeSchema = new mongoose.Schema({
     owner: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
     title: String,
     address: String,
-    photo: [String],
+    photos: [String],
     description: String,
     perks: [String],
     extraInfo: String,
-    chechIn: Number,
+    checkIn: Number,
     checkOut: Number,
-    maxGuest: Number,
+    maxGuests: Number,
 })
 
 const PlaceModel = mongoose.model('Place', placeSchema)
